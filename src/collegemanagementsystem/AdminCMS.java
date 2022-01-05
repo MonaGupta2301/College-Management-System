47b524f0cbe9031e9640f18e1a430da9be5921d5
Mona Gupta <84773046+MonaGupta2301@users.noreply.github.com>
2022-01-05 11:06:02 +0530

    Update AdminCMS.java

--- src/collegemanagementsystem/AdminCMS.java
+++ src/collegemanagementsystem/AdminCMS.java
@@ -35,6 +35,8 @@ public class AdminCMS extends javax.swing.JFrame {
 
      }
     @SuppressWarnings("unchecked")
+
+
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
